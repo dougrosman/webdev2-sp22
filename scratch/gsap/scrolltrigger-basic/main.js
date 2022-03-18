150bddd4959a128ea817ac9a2552dc1d6a35861c
Doug Rosman <drosman@saic.edu>
2022-03-18 12:34:27 -0500

    update scroll trigger

---
+++ scratch/gsap/scrolltrigger-basic/main.js
@@ -0,0 +1,16 @@
+// let tl = gsap.timeline();
+
+// tl.to(".a", {right: '0', background: 'red', duration: 2})
+//   .to(".a", {bottom: '0', background: 'green', duration: 1})
+//   .to(".b", {bottom: '0', background: 'purple', duration: .5})
+
+gsap.to('.box', {
+  scrollTrigger: {
+    trigger: ".divider",
+    start: "top center",
+    toggleActions: "play pause none none"
+  },
+  x: 800,
+  rotation: 360,
+  duration: 3
+})
